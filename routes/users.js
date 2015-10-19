e34e360155c3a6a20fee98dcd5519e01ae31ca2f
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-19 13:20:53 -0600

    signup validation is complete

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,47 @@ router.get('/signup', function(req, res, next) {
   res.render('signup', { title: 'Sign Up' });
 });
 
-router.post('/signup', function (req, res, next) {
+
+router.post('/signin', function (req, res) {
+  var errors = [];
   var hash = bcrypt.hashSync(req.body.password, 8);
   req.session.email = req.body.email;
-  userdb.insert({email: req.session.email, password: hash}, function (err, credentials) {
-    res.redirect('success');
-  });
-});
+
+  if (!req.body.email.trim()) {
+    errors.push("* email can't be blank")
+  }
+  if (!req.body.password.trim()) {
+    errors.push("* password can't be blank")
+  }
+  if (!req.body.passwordv.trim()) {
+    errors.push("* verify password can't be blank")
+  }
+  if (req.body.passwordv.trim() !== req.body.password.trim()) {
+    errors.push("* password inputs don't match")
+  }
+  if (errors.length>0) {
+    console.log(errors);
+    res.render('signup', {errors: errors})
+  } else {
+    console.log(errors);
+    userdb.insert({email: req.session.email, password: hash}, function (err, credentials) {
+      res.redirect('success');
+    })
+  }
+})
+
+
+// router.post('/signup', function (req, res, next) {
+//   var hash = bcrypt.hashSync(req.body.password, 8);
+//   req.session.email = req.body.email;
+//   if (req.body.email.trim()) {
+//     userdb.insert({email: req.session.email, password: hash}, function (err, credentials) {
+//       res.redirect('success');
+//     });
+//   } else {
+//     res.render('signup', {errors: ["* Email can't be blank"]})
+//   }
+// });
 
 router.get('/success', function (req, res, next) {
   res.render('success', {title: 'SUCCESS!', email: req.session.email})
