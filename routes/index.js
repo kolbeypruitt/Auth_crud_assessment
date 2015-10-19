51ad690e7b8216de41a601c7f713ce08923b7608
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-19 11:24:11 -0600

    crud almost complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/studentdb');
+var studentdb = db.get('students');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'please sign up or login' });
 });
 
+router.get('/students', function(req, res, next) {
+  studentdb.find({}, function (err, records) {
+    res.render('students/index', {allStudents: records});
+  });
+});
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -7,14 +7,30 @@ var studentdb = db.get('students');
 
 /* GET students listing. */
 router.get('/', function(req, res, next) {
-  res.render('students', { title: 'students page' });
+  res.render('students/index', { title: 'students page' });
 });
 
 router.post('/', function(req, res, next) {
-  studentCollection.insert({ name: req.body.student_name, phone: req.body.student_phone }, function(err, student) {
+  studentdb.insert({ name: req.body.student_name, phone: req.body.student_phone }, function(err, student) {
     studentdb.find({}, function (err, records) {
       res.render('students/index', {allStudents: records});
     });
+  });
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('students/add');
+});
+
+router.get('/:id', function(req, res, next) {
+  studentdb.findOne({_id: req.params.id}, function (err, record) {
+    res.render('students/show', {theStudent: record});
+  });
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  studentdb.findOne({_id: req.params.id}, function (err, record) {
+    res.render('students/edit', {theStudent: record});
   });
 });
 
