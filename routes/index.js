3c3b7f8b054e6b0c666533b2fd5b30f9b8822c45
kolbeypruitt <kolbeypruitt@gmail.com>
2015-10-19 09:32:16 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Sign Up' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login' });
+});
+module.exports = router;
